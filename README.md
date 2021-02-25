139feb504517c0958cfb41b504886eb9d898e8e5
soyeon <sypark9646@gmail.com>
2021-02-25 19:11:17 +0900

    initialize project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # java-json-parser
+
 jackson을 활용하여 json을 특정 object로 맵핑 해보자
---
+++ src/main/java/com/example/jsonparser/JsonUtils.java
@@ -0,0 +1,9 @@
+package com.example.jsonparser;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class JsonUtils {
+
+}
